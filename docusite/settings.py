2c3cd29d4e01eee72c4c290fbc508e4fde81c56a
Sebastian Villarreal <sebastian@elbasti.com>
2013-07-21 23:18:19 -0500

    Fixed? Buggy heroku settings

--- docusite/settings.py
+++ docusite/settings.py
@@ -4,6 +4,18 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -65,6 +77,9 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
+
+STATICFILES_DIRS = (
+        )
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
