6290e3d3e48ae732b9990979f3d47d8020917c27
Sebastian Villarreal <sebastian@elbasti.com>
2013-07-22 15:57:32 -0500

    shimmed db config so that it skips assignment if empty (heroku)

--- docusite/settings.py
+++ docusite/settings.py
@@ -7,7 +7,7 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
@@ -159,7 +159,10 @@ LOGGING = {
 
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
+dbconfig = dj_database_url.config()
+if dbconfig:
+    DATABASES['default'] = dbconfig 
 
 #Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
